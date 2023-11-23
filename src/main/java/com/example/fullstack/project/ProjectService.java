226f672dd67b87fa569246951739a878718efd75
Fabio Tosques <ft@hMBP-von-Fabio>
2023-11-23 21:48:08 +0100

    Added: ProjectService and TaskService.

--- src/main/java/com/example/fullstack/project/ProjectService.java
+++ src/main/java/com/example/fullstack/project/ProjectService.java
@@ -1,10 +1,14 @@
 package com.example.fullstack.project;
 
+import java.util.List;
+
 import org.hibernate.ObjectNotFoundException;
 
+import com.example.fullstack.task.Task;
 import com.example.fullstack.user.UserService;
 
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
+import io.quarkus.security.UnauthorizedException;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -24,6 +28,42 @@ public class ProjectService {
         return userService.getCurrentUser()
           .chain(user -> Project.<Project>findById(id)
           .onItem().ifNull().failWith(() ->
-            new ObjectNotFoundException(id, "Project")));
+            new ObjectNotFoundException(id, "Project"))
+            .onItem().invoke(project -> {
+                if (!user.equals(project.user)) {
+                    throw new UnauthorizedException(
+                        "You are not allowed to update this project"
+                    );
+                }
+            }));
+    }
+
+    @WithTransaction
+    public Uni<List<Project>> listForUser() {
+        return userService.getCurrentUser()
+          .chain(user -> Project.find("user", user).list());
+    }
+
+    @WithTransaction
+    public Uni<Project> create(Project project) {
+        return userService.getCurrentUser()
+          .chain(user -> {
+            project.user = user;
+            return project.persistAndFlush();
+          });
+    }
+
+    @WithTransaction
+    public Uni<Project> update(Project project) {
+        return findById(project.id)
+          .chain(p -> Project.getSession())
+          .chain(s -> s.merge(project));
+    }
+
+    @WithTransaction
+    public Uni<Void> delete(long id) {
+        return findById(id)
+          .chain(p -> Task.update("project = null where project = ?1", p)
+            .chain(i -> p.delete()));
     }
 }
