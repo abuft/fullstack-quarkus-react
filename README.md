e49aecfb25cdef8d7022f15868fb299442279f80
Fabio Tosques <ft@hMBP-von-Fabio>
2023-11-26 19:49:14 +0100

    Added: Auth services

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # reactive Project
 
+Project URL: https://github.com/PacktPublishing/Full-Stack-Quarkus-and-React/
+
 This project uses Quarkus, the Supersonic Subatomic Java Framework.
 
 If you want to learn more about Quarkus, please visit its website: https://quarkus.io/ .
--- src/main/java/com/example/fullstack/user/UserService.java
+++ src/main/java/com/example/fullstack/user/UserService.java
@@ -2,6 +2,7 @@ package com.example.fullstack.user;
 
 import java.util.List;
 
+import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.hibernate.ObjectNotFoundException;
 
 import com.example.fullstack.project.Project;
@@ -11,10 +12,20 @@ import io.quarkus.elytron.security.common.BcryptUtil;
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.ClientErrorException;
+import jakarta.ws.rs.core.Response;
 
 @ApplicationScoped
 public class UserService {
     
+    private final JsonWebToken jwt;
+
+    @Inject
+    public UserService(JsonWebToken jwt) {
+        this.jwt = jwt;
+    }
+
     @WithTransaction
     public Uni<User> findById(long id) {
         return User.<User>findById(id)
@@ -57,6 +68,23 @@ public class UserService {
 
     public Uni<User> getCurrentUser() {
         // ToDo: replace implementation once security is added to the project
-        return User.find("order by id").firstResult();
+        // return User.find("order by id").firstResult();
+        return findByName(jwt.getName());
+    }
+
+    public static boolean matches(User user, String password) {
+        return BcryptUtil.matches(password, user.password);
+    }
+
+    @WithTransaction
+    public Uni<User> changePassword(String currentPassword, String newPassword) {
+        return getCurrentUser()
+          .chain(u -> {
+            if (!matches(u, currentPassword)) {
+                throw new ClientErrorException("Current password does not match", Response.Status.CONFLICT);
+            }
+            u.setPassword(BcryptUtil.bcryptHash(newPassword));
+            return u.persistAndFlush();
+          });
     }
 }
