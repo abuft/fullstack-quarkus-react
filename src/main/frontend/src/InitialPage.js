a9bb998c7e965ac77e887f9a9c6bb0b86b327a81
Fabio Tosques <ft@hMBP-von-Fabio>
2023-12-14 17:16:50 +0100

    Added React InitialPage.js file.

--- src/main/frontend/src/InitialPage.js
+++ src/main/frontend/src/InitialPage.js
@@ -5,7 +5,7 @@ import {Layout} from './layout';
 export const InitialPage = () => (
   <Layout>
     <Typography variant='h4' >
-      Greetings Professor Falken!
+      Display a Dummy Page :)
     </Typography>
   </Layout>
 );
